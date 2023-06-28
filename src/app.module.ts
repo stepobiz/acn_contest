12a1f30a17ed026efea746d4ef1a7e222973eab4
Cavallaro Catello Stefano <stefano@stepo.biz>
2023-06-28 15:04:07 +0200

    Fix config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AcnContextModule } from './acn-context/acn-context.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		AcnContextModule,
+	],
+	controllers: [
+		AppController,
+
+	],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
